c3b085a08fa5a435610d8729d04707b6f708e6fb
raulrequenac <raulrequec@gmail.com>
2020-04-28 21:54:50 +0200

    cors fixed for real

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const nodemailer = require('nodemailer');
 const cors = require('cors')
 
 const app = express()
-app.use(cors)
+app.use(corsMiddleware)
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use('/', router)
